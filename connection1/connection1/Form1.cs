7532556e3432461f2d684126f809a9ba60516f9c
Guochen Sun <guochen_s@hotmail.com>
2016-09-14 14:04:46 +0800

    connection_text succeed!

---
+++ connection1/connection1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace connection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string constr = "server=localhost;User Id=root;password=test;Database=reg";
+            MySqlConnection mycon = new MySqlConnection(constr);
+            mycon.Open();
+            MySqlCommand mycmd = new MySqlCommand("insert into test(id) values('233')", mycon);
+            if (mycmd.ExecuteNonQuery() > 0)
+            {
+                Console.WriteLine("数据插入成功！");
+            }
+            Console.ReadLine();
+            mycon.Close();
+        }
+    }
+}
---
+++ connection1/connection1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Web;
+
+namespace connection1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
